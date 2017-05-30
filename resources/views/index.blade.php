e3c4d24b0523d8fbe9c1371551b251871ddb7843
sanket2108 <sanketpatel2108@gmail.com>
2017-05-30 23:50:18 +0530

    Profile Page Setup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,101 @@
 @extends('header')
 @section('content')
 
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="main-profile">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-5 profile-left-box">
+                            <div class="profile-box">
+                                <div class="profile-image">
+                                    <img src="img/PicsArt_11-14-12.30.45.jpg" class="img-responsive center-block">
+                                </div>
+                                <h1 class="profile-name">Sanket Patel</h1>
+                                <div class="bottom-space">
+                                    <ul class="list-inline follwers text-center">
+                                        <li><a href="#"> <span> 10 </span> follwers </a></li>
+                                        <li><a href="#"> <span> 15 </span> followings </a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="bottom-space">
+                                    <h4 class="h4-title">DOB:</h4>
+                                    <p>21-08-1994</p>
+                                </div>
+                                <div class="bottom-space">
+                                    <h4 class="h4-title">Phone:</h4>
+                                    <p>7405094570</p>
+                                </div>
+                                <div class="bottom-space">
+                                    <h4 class="h4-title">Email:</h4>
+                                    <p>sanketpatel2108@gmail.com</p>
+                                </div>
+                                <div class="bottom-space">
+                                    <h4 class="h4-title">Website:</h4>
+                                    <p>www.knowamp.com</p>
+                                </div>
+                                <div class="bottom-space">
+                                <h4 class="h4-title">Social Connections:</h4>
+                                    <ul class="list-unstyled social-connections">
+                                        <li>
+                                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i><span>facebook.com/sanketpatel</span></a>
+                                        </li>
+                                        <li>
+                                            <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i><span>plus.google.com/sanketpatel</span></a>
+                                        </li>
+                                        <li>
+                                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i><span>twitter.com/sanketpatel</span></a>
+                                        </li>
+                                        <li>
+                                            <a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i><span>pinterest.com/sanketpatel</span></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-9 col-sm-7 profile-right-box">
+                            <div class="bottom-space">
+                                <h3 class="h3-title">About Me</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia placerat diam quis molestie. Donec in consequat eros. Nullam quis diam augue. Sed ac tincidunt ante. Sed quis tellus in libero malesuada tincidunt. Sed quis porta augue. Nam odio dolor, lobortis eget elit eget, rutrum fringilla dui. Maecenas tempus sapien ut dui scelerisque, nec congue nisl molestie.
+                                </p>
+                            </div>
+                            <div class="bottom-space">
+                                <h3 class="h3-title">Work & Education</h3>
+                                <div class="row">                                        
+                                    <div class="col-xs-12 col-sm-4 col-md-2">
+                                        <h4 class="h4-title">Workspaces: </h4>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-8 col-md-10">
+                                        <div class="work-box space10">
+                                            <div class="current-work">
+                                            	<h5><span> Current: </span></h5>
+                                                <p> Web Designer at WEBMAVENS PVT. LTD., India. </p>
+                                            </div>
+                                            <div class="past-work">
+                                            	<h5><span> Past: </span></h5>
+                                                <ul class="list-unstyled">
+                                                    <li>Web designer at dhrup tech.</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                	<div class="col-xs-12 col-sm-4 col-md-2">
+                                		<h4 class="h4-title"> Education: </h4>
+                                	</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
